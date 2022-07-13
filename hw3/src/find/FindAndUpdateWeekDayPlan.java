8c9e7a9e572ebb967c325dad4098552a499a22ad
talibnabi <talibnabiyev@gmail.com>
2022-07-13 03:57:26 -0700

    homework

--- hw3/src/find/FindAndUpdateWeekDayPlan.java
+++ hw3/src/find/FindAndUpdateWeekDayPlan.java
@@ -10,27 +10,13 @@ public class FindAndUpdateWeekDayPlan {
             String input = inputHandler("update");
             getText("Please, input new tasks for " + input + ".");
             switch (input) {
-                case "sunday":
-                    schedule[0][1] = scanner.nextLine();
-                    break;
-                case "monday":
-                    schedule[1][1] = scanner.nextLine();
-                    break;
-                case "tuesday":
-                    schedule[2][1] = scanner.nextLine();
-                    break;
-                case "wednesday":
-                    schedule[3][1] = scanner.nextLine();
-                    break;
-                case "thursday":
-                    schedule[4][1] = scanner.nextLine();
-                    break;
-                case "friday":
-                    schedule[5][1] = scanner.nextLine();
-                    break;
-                case "saturday":
-                    schedule[6][1] = scanner.nextLine();
-                    break;
+                case "sunday" -> schedule[0][1] = scanner.nextLine();
+                case "monday" -> schedule[1][1] = scanner.nextLine();
+                case "tuesday" -> schedule[2][1] = scanner.nextLine();
+                case "wednesday" -> schedule[3][1] = scanner.nextLine();
+                case "thursday" -> schedule[4][1] = scanner.nextLine();
+                case "friday" -> schedule[5][1] = scanner.nextLine();
+                case "saturday" -> schedule[6][1] = scanner.nextLine();
             }
         }
     }
