6b6eb9b0367534044580d50a122c9e93573b5d3b
talibnabi <talibnabiyev@gmail.com>
2022-07-17 01:12:06 -0700

    homework

--- hw4/src/simpleProject/alive/Pet.java
+++ hw4/src/simpleProject/alive/Pet.java
@@ -1,4 +1,21 @@
 package simpleProject.alive;
 
 public class Pet {
+    String species;
+    String nickname;
+    Integer age;
+    Integer trickLevel1;
+    String[] habits;
+
+    void eat() {
+        System.out.println("I am eating");
+    }
+
+    void respond() {
+        System.out.println("Hello, owner. I am " + this.nickname + ". I miss you!");
+    }
+
+    void foul() {
+        System.out.println("I need to cover it up");
+    }
 }
