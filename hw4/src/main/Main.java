a03a3eb0884b4a4bb2e5d82a0b5420569d293057
talibnabi <talibnabiyev@gmail.com>
2022-07-17 06:05:17 -0700

    homeworks

--- hw4/src/main/Main.java
+++ hw4/src/main/Main.java
@@ -5,7 +5,18 @@ import alive.Pet;
 
 public class Main {
     public static void main(String[] args) {
-      /*  Pet pet = new Pet();
+        petDoIt1();
+        petDoIt2();
+        petDoIt3();
+        humanDoIt1();
+        humanDoIt2();
+        humanDoIt3();
+        humanDoIt4();
+    }
+
+
+    private static void petDoIt1() {
+        Pet pet = new Pet();
         pet.species = "dog";
         pet.nickname = "alisa";
         pet.age = 13;
@@ -14,20 +25,83 @@ public class Main {
         habits[0] = "swimming";
         habits[1] = "volleyball";
         habits[2] = "football";
-        pet.habits = habits;*/
-//        System.out.println(pet.toString());
-/*        Human father = new Human();
+        pet.habits = habits;
+        System.out.println(pet.toString());
+        pet.eat();
+        pet.foul();
+        pet.respond();
+    }
+
+    private static void petDoIt2() {
+        Pet pet = new Pet("dog", "alisa");
+        pet.age = 13;
+        pet.trickLevel1 = 90;
+        String[] habits = new String[3];
+        habits[0] = "swimming";
+        habits[1] = "volleyball";
+        habits[2] = "football";
+        pet.habits = habits;
+        System.out.println(pet.toString());
+        pet.eat();
+        pet.foul();
+        pet.respond();
+    }
+
+    public static void petDoIt3() {
+        String[] habits = new String[3];
+        habits[0] = "swimming";
+        habits[1] = "volleyball";
+        habits[2] = "football";
+        Pet pet = new Pet("dog", "alisa", 2, 85, habits);
+        System.out.println(pet.toString());
+        pet.eat();
+        pet.foul();
+        pet.respond();
+    }
+
+    public static void humanDoIt1() {
+        Pet pet = new Pet();
+        pet.species = "dog";
+        pet.nickname = "alisa";
+        pet.age = 13;
+        pet.trickLevel1 = 90;
+        String[] habits = new String[3];
+        habits[0] = "swimming";
+        habits[1] = "volleyball";
+        habits[2] = "football";
+        pet.habits = habits;
+//        pet.eat();
+//        pet.foul();
+//        pet.respond();
+        Human father = new Human();
         father.name = "Salami";
         father.surname = "Ghahramani";
         Human mother = new Human();
         mother.name = "Elsa";
         mother.surname = "Ghahramani";
         String[][] schedule = {{"Monday", "go to gym"}};
-        Human human = new Human("Teresa", "Ghahramani", 2001, 90, pet, father, mother, schedule);
-        System.out.println(human.toString());*/
+        Human child = new Human("Teresa", "Ghahramani", 2001, 90, pet, father, mother, schedule);
+        System.out.println(child.toString());
 
+        boolean check = child.feedPet(true);
+        System.out.println(check);
+        child.describePet();
+        child.greetPet();
+    }
 
-        /*Human human = new Human();
+    private static void humanDoIt2() {
+        Pet pet = new Pet("dog", "alisa");
+        pet.age = 13;
+        pet.trickLevel1 = 90;
+        String[] habits = new String[3];
+        habits[0] = "swimming";
+        habits[1] = "volleyball";
+        habits[2] = "football";
+        pet.habits = habits;
+//        pet.eat();
+//        pet.foul();
+//        pet.respond();
+        Human child = new Human();
         Human father = new Human();
         father.name = "Albert";
         father.surname = "Moreno";
@@ -35,49 +109,71 @@ public class Main {
         mother.name = "Alisa";
         mother.surname = "Moreno";
         String[][] schedule = {{"Monday", "go to gym"}};
-        human.name = "Alberto";
-        human.surname = "Moreno";
-        human.year = 2001;
-        human.iq = 99;
-        human.pet = pet;
-        human.father = father;
-        human.mother = mother;
-        human.schedule = schedule;
-        System.out.println(human.toString());*/
+        child.name = "Alberto";
+        child.surname = "Moreno";
+        child.year = 2001;
+        child.iq = 99;
+        child.pet = pet;
+        child.father = father;
+        child.mother = mother;
+        child.schedule = schedule;
+        System.out.println(child.toString());
+        boolean check = child.feedPet(true);
+        System.out.println(check);
+        child.describePet();
+        child.greetPet();
+    }
 
-
-      /*  Human father = new Human();
+    private static void humanDoIt3() {
+        String[] habits = new String[3];
+        habits[0] = "swimming";
+        habits[1] = "volleyball";
+        habits[2] = "football";
+        Pet pet = new Pet("dog", "alisa", 2, 85, habits);
+//        pet.eat();
+//        pet.foul();
+//        pet.respond();
+        Human father = new Human();
         father.name = "Ramos";
         father.surname = "Suarez";
         Human mother = new Human();
         mother.name = "Jasmin";
         mother.surname = "Suarez";
         String[][] schedule = {{"Monday", "go to gym"}};
-        Human human = new Human("Teresa", "Suarez", 2001, pet, father, mother);
-        System.out.println(human.toString());*/
+        Human child = new Human("Teresa", "Suarez", 2001, pet, father, mother);
+        child.schedule = schedule;
+        System.out.println(child.toString());
+        boolean check = child.feedPet(true);
+        System.out.println(check);
+        child.describePet();
+        child.greetPet();
+    }
 
-
-        Pet pet = new Pet();
-        pet.species = "dog";
-        pet.nickname = "suarez";
-        pet.age = 2;
-        pet.trickLevel1 = 90;
+    public static void humanDoIt4() {
         String[] habits = new String[3];
         habits[0] = "swimming";
         habits[1] = "volleyball";
         habits[2] = "football";
-        pet.habits = habits;
+        Pet pet = new Pet("dog", "alisa", 2, 85, habits);
+//        pet.eat();
+//        pet.foul();
+//        pet.respond();
         Human father = new Human();
-        father.name = "Simon";
-        father.surname = "Alberto";
+        father.name = "Ramos";
+        father.surname = "Suarez";
         Human mother = new Human();
-        mother.name = "Alisa";
-        mother.surname = "Alberto";
-        String[][] schedule = {{"Monday", "go to gym"}, {"Wednesday", "go to swimming"}};
-        Human child = new Human("Salman", "Alberto", 2001, 90, pet, father, mother, schedule);
+        mother.name = "Jasmin";
+        mother.surname = "Suarez";
+        String[][] schedule = {{"Monday", "go to gym"}};
+        Human child = new Human("Teresa", "Suarez", 2001);
+        child.pet = pet;
+        child.father = father;
+        child.mother = mother;
+        child.schedule = schedule;
         System.out.println(child.toString());
-
-        boolean b=child.feedPet(true);
-        System.out.println(b);
+        boolean check = child.feedPet(true);
+        System.out.println(check);
+        child.describePet();
+        child.greetPet();
     }
 }
