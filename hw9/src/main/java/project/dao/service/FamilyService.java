ce61065adb267677d7eabc7d37c866144b42a615
talibnabi <talibnabiyev@gmail.com>
2022-08-19 02:52:06 -0700

    commit

--- hw9/src/main/java/project/dao/service/FamilyService.java
+++ hw9/src/main/java/project/dao/service/FamilyService.java
@@ -1,5 +1,39 @@
 package project.dao.service;
 
+import project.dao.data.CollectionFamilyDao;
+import project.dao.inter.FamilyDao;
+import project.main.Family;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FamilyService {
+    private final FamilyDao familyDao;
+
+    public FamilyService(CollectionFamilyDao collectionFamilyDao) {
+        this.familyDao = collectionFamilyDao;
+    }
+
+    public List<Family> getAllFamilies() {
+        return this.familyDao.getAllFamilies();
+    }
+
+    public void displayAllFamilies() {
+        this.familyDao
+                .getAllFamilies()
+                .forEach(family ->
+                        System.out.printf("%d %s \n", getAllFamilies()
+                                .indexOf(family), family));
+    }
+
+    public List<Family> getFamiliesBiggerThan(int size) {
+        List<Family> familiesBiggerThan = this.familyDao
+                .getAllFamilies()
+                .stream()
+                .filter(family -> family.countFamily() > size)
+                .toList();
+        System.out.println(familiesBiggerThan);
+        return familiesBiggerThan;
+    }
     
 }
