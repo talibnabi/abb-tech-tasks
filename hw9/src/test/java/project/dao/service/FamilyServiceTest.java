358623384b99027855e5485643a73676c4254243
talibnabi <talibnabiyev@gmail.com>
2022-08-19 08:08:25 -0700

    commit

--- hw9/src/test/java/project/dao/service/FamilyServiceTest.java
+++ hw9/src/test/java/project/dao/service/FamilyServiceTest.java
@@ -1,5 +1,70 @@
 package project.dao.service;
 
-public class FamilyServiceTest {
+import org.testng.annotations.Test;
 
+public class FamilyServiceTest {
+    @Test
+    void getAllFamiliesTest() {
+
+    }
+
+    @Test
+    void displayAllFamiliesTest() {
+
+    }
+
+    @Test
+    void getFamiliesBiggerThanTest() {
+
+    }
+
+    @Test
+    void getFamiliesLessThanTest() {
+
+    }
+
+    @Test
+    void countFamiliesWithMemberNumberTest() {
+
+    }
+
+    @Test
+    void createNewFamilyTest() {
+
+    }
+
+    @Test
+    void deleteFamilyByIndexTest() {
+
+    }
+
+    @Test
+    void bornChildTest() {
+
+    }
+
+    @Test
+    void adoptChildTest() {
+
+    }
+
+    @Test
+    void deleteAllChildrenOlderThenTest() {
+
+    }
+
+    @Test
+    void countTest() {
+
+    }
+
+    @Test
+    void getFamilyByIdTest() {
+
+    }
+
+    @Test
+    void getPetsTest() {
+
+    }
 }
