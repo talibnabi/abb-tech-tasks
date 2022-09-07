4a4040a349ee50c06b353cb827ae0984a2c42eaf
talibnabi <talibnabiyev@gmail.com>
2022-09-07 02:40:16 -0700

    commit

--- hw13/src/main/java/project/datasource/DataSource.java
+++ hw13/src/main/java/project/datasource/DataSource.java
@@ -2,18 +2,14 @@ package project.datasource;
 
 import project.model.human.Family;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataSource {
-    private final List<Family> families = new ArrayList<>();
+    private List<Family> families = new ArrayList<>();
 
-
-    protected void writeFamilyToFile() throws FileNotFoundException {
+    public void writeFamilyToFile() throws FileNotFoundException {
         String fileName = "family.bin";
         File file = new File(fileName);
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file, true))) {
@@ -21,5 +17,17 @@ public class DataSource {
         } catch (Exception ignored) {
             throw new FileNotFoundException("File not found.");
         }
+    }
+
+    public List<Family> readFamilyFromFile() throws FileNotFoundException {
+        String fileName = "family.bin";
+        File file = new File(fileName);
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+            families = (List<Family>) objectInputStream.readObject();
+
+        } catch (Exception ignored) {
+            throw new FileNotFoundException("File not found.");
+        }
+        return families;
     }
 }
