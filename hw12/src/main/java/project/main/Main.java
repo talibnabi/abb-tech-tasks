33aa0be91354f75e5f27368678a3d6a2a888ece6
talibnabi <talibnabiyev@gmail.com>
2022-09-07 01:51:20 -0700

    commit

--- hw12/src/main/java/project/main/Main.java
+++ hw12/src/main/java/project/main/Main.java
@@ -1,13 +1,42 @@
 package project.main;
 
 
+import project.model.enumeration.Species;
+import project.model.human.Family;
+import project.model.human.Human;
+import project.model.pet.Dog;
+import project.model.pet.Pet;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 import static project.main.Menu.*;
 
 
 public class Main {
     public static void main(String[] args) throws Throwable {
         showMenu();
+//        prettyExample();
     }
+
+    private static void prettyExample() throws ParseException {
+        Human tahmina = new Human("Tahmina", "Muzaffar", "10/02/2001", 99, new HashMap<>());
+        Human alisa = new Human("Alisa", "Aliqummatov", "12/02/2001", 15, new HashMap<>());
+        Pet pet = new Dog();
+        pet.setNickname("x man");
+        pet.setAge(19);
+        pet.setHabits(new HashSet<>());
+        pet.setTrickLevel1(23);
+        pet.setSpecies(Species.DOG);
+        Set<Pet> pets = new HashSet<>();
+        pets.add(pet);
+        Family family = new Family(tahmina, alisa);
+        family.setPets(pets);
+        System.out.println(family.prettyFormat());
+    }
+
 
 //    private static void humanEnumShow() {
 //        Human human = new Human();
