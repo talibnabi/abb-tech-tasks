d388326866632a0ba058949097b62072ed53c3ac
talibnabi <talibnabiyev@gmail.com>
2022-09-16 03:46:48 -0700

    commit

--- step-project-booking-app/src/util/FileUtil.java
+++ step-project-booking-app/src/util/FileUtil.java
@@ -6,7 +6,7 @@ import db.file.concretes.WorkingWithFileForID;
 
 public class FileUtil {
     public static final FileWorkingID workingWithFileForID = new WorkingWithFileForID();
-    public static final String passenger = "passengerID.txt";
+    public static final String passenger = "db/id/passengerID.txt";
 
     public static Boolean checkFile(String filePath) {
         if (workingWithFileForID.readAllIndexFromFile(filePath).isEmpty()) {
